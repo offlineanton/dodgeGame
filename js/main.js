8430c78ba5480141927ea8b9ca99b615926229f8
Anton <offlineanton@hotmail.co.uk>
2016-01-28 15:45:06 +0000

    Stuck in the box :black_large_square:
    You can't leave the box anymore

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ var activeKeys = [];
 var player = $("#player");
 var gameArea = $("#gameArea");
 
+//dimension of game area
+
+var gameAreaRight = gameArea.offset().left + gameArea.width();
+var gameAreaLeft = gameArea.offset().left;
+var gameAreaTop = gameArea.offset().top;
+var gameAreaBottom = gameArea.offset().top + gameArea.height();
+
+
 // List of keyCodes I might use
 
 /*
@@ -32,32 +40,55 @@ $(window).on("keyup",function(e){
   activeKeys[e.keyCode] = false;
 })
 
+
+//the whole game loop
 playLoop = setInterval(function(){
+
+  //get position of player
+  playerRight = player.offset().left + player.width();
+  playerLeft = player.offset().left;
+  playerTop = player.offset().top;
+  playerBottom = player.offset().top + player.height();
+
+
+
   if(activeKeys[39] || activeKeys[68]){
-    move('right');
+    if(gameAreaRight > playerRight){
+      move('right');
+    }
   }
+
   if(activeKeys[37] || activeKeys[65]){
-    move('left');
+    if(gameAreaLeft < playerLeft){
+      move('left');
+    }
   }
+
   if(activeKeys[38] || activeKeys[87]){
-    move('up');
+    if(gameAreaTop < playerTop){
+      move('up');
+    }
   }
   if(activeKeys[40] || activeKeys[83]){
-    move('down');
+    if(gameAreaBottom > playerBottom){
+      move('down');
+    }
   }
 })
 
+
+//moves character when a key is active
 function move(direction){
   if(direction == "up"){
-    player.css({"top": "-=1px"});
+    player.css({"top": "-=1.5px"});
   }
   if(direction == "right"){
-    player.css({"left": "+=1px"});
+    player.css({"left": "+=1.5px"});
   }
   if(direction == "down"){
-    player.css({"top": "+=1px"});
+    player.css({"top": "+=1.5px"});
   }
   if(direction == "left"){
-    player.css({"left": "-=1px"});
+    player.css({"left": "-=1.5px"});
   }
 }
